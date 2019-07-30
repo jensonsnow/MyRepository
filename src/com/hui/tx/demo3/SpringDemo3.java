a5ece364e0ee24e61738bc50f59819f614c701ce
辉 <@DESKTOP-ONAEJS9>
2019-07-30 11:39:02 +0800

    spring事务练习

--- src/com/hui/tx/demo3/SpringDemo3.java
+++ src/com/hui/tx/demo3/SpringDemo3.java
@@ -8,8 +8,8 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration("classpath:applicationContext2.xml")
-public class SpringDemo2 {
+@ContextConfiguration("classpath:applicationContext3.xml")
+public class SpringDemo3 {
      
 	@Resource(name="accountService")
 	private AccountService accountService;
